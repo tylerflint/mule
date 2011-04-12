6eee2e333b550ef093cdb68dd0aa8eaac0a03826
tylerflint <tylerflint@gmail.com>
2011-04-12 00:25:32 -0600

    adding functionality into grandmaster for starting; exec'ing child; and signal handling

--- lib/mule/grandmaster.rb
+++ lib/mule/grandmaster.rb
@@ -1,36 +1,65 @@
 module Mule
   class Grandmaster
     
+    CHILDREN = []
+    
     SIG_QUEUE = []
     
     QUEUE_SIGS = [:QUIT, :INT, :TERM, :USR2]
     
+    def initialize(config_file)
+      @config_file = config_file
+    end
+    
     def start
-      # create configurator instance
-      
-      # start master
-      
       # trap sigs
       QUEUE_SIGS.each do |sig|
-        trap(sig) {SIG_QUEUE << sig; wakeup!}
+        trap(sig) {SIG_QUEUE << sig; wakeup}
       end
-      
-      # sleep forever
+      exec_child
       sleep
     end
     
-    def wakeup!
+    def exec_child
+      # create configurator instance
+      configurator = Configurator.new(@config_file).parse!
+      
+      # start master
+      pid = fork do
+        Master.new(configurator).start
+      end
+      CHILDREN << pid
+      pid
+    end
+    
+    def wakeup
       case SIG_QUEUE.shift
-      when nil
-        # wtf!?
       when :INT, :TERM
+        reap_children
         exit
       when :QUIT
-        # graceful shutdown
+        reap_children(true)
+        exit
       when :USR2
-        # reload
+        new_child = exec_child
+        reap_children(true, [new_child])
+        sleep
       end
-      sleep
+    end
+    
+    def reap_children(graceful=false, grant_amnesty=[])
+      CHILDREN.each do |pid|
+        begin
+          unless grant_amnesty.include?(pid)
+            Process.kill((graceful): :QUIT ? :TERM , pid)
+            Process.detach(pid)
+            CHILDREN.delete(pid)
+          end
+        rescue Errno::ESRCH, Errno::ENOENT
+          # do nothing, we don't care if were missing a pid that we're
+          # trying to murder already
+        end
+      end
     end
     
   end
